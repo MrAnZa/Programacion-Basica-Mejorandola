acefbf41f37fb04605b4bf35903471e416071253
KevinJM <undefined.hlo.o@gmail.com>
2014-08-06 09:08:17 -0430

    Clase 1

--- Marte/codigo.js
+++ Marte/codigo.js
@@ -1,18 +1,17 @@
+// Un simple codigo para saber cuál sería tu peso en marte
+// Declaramos variables
 var peso;
 var pesoEnMarte;
 
+// Con alert le preguntamos al usuario
 alert("¿Quieres saber tu peso en marte?");
 
+// Con prompt volvemos a preguntar al usuario, esta vez esperando una respuesta
 peso = prompt("¿Cuál es tu peso en kg?","70");
 peso = parseInt(peso);
 
+// Hacemos el calculo del peso
 pesoEnMarte = (peso / 9.81) * 3.711;
 
+//Luego mostramos.
 alert(pesoEnMarte);
-
-//var nombre = "Kevin";
-//var apellido = "Morales";
-//var edad = 22;
-
-//alert(nombre + " " + apellido + "\n" + edad + "años.");
-//alert ("2" + 5 * 8);
